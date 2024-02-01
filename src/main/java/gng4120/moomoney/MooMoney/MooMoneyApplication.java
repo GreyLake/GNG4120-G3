f1a3c37e4c435818c255c2494d8841520acb282c
BuildTools <unconfigured@null.spigotmc.org>
2024-01-31 20:20:12 -0500

    Initial Commit

---
+++ src/main/java/gng4120/moomoney/MooMoney/MooMoneyApplication.java
@@ -0,0 +1,13 @@
+package gng4120.moomoney.MooMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MooMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gng4120/moomoney/MooMoney/MooMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package gng4120.moomoney.MooMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
