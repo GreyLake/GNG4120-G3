5839c937809f2fb982f3cec03f36ca6fa5bff941
GreyLake <bigrasj3@gmail.com>
2024-02-01 16:21:14 -0500

    Adding Security Roles

--- src/main/java/gng4120/group3/project/models/ERole.java
+++ src/main/java/gng4120/group3/project/models/ERole.java
@@ -1,4 +1,7 @@
 package gng4120.group3.project.models;
 
-public class ERole {
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
 }
--- src/main/java/gng4120/group3/project/models/Role.java
+++ src/main/java/gng4120/group3/project/models/Role.java
@@ -1,4 +1,8 @@
 package gng4120.group3.project.models;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "roles")
 public class Role {
+
 }
