01ad10deead0a9a29edcace430a9bae3edb4fe1f
GreyLake <bigrasj3@gmail.com>
2024-03-02 19:13:51 -0500

    Added Exception Handling for MongoDB

--- src/main/java/gng4120/group3/project/database/MongoConfig.java
+++ src/main/java/gng4120/group3/project/database/MongoConfig.java
@@ -3,8 +3,11 @@ package gng4120.group3.project.database;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
+import com.mongodb.MongoSocketException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.internal.MongoClientImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
@@ -30,6 +33,8 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
     @Value("${MONGODB_PASSWORD}")
     private String PASS;
 
+    private boolean isMongoDBAvailable = false;
+
     @Override
     protected String getDatabaseName() {
         return DB;
@@ -45,12 +50,30 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
          *         .build();
          */
 
-        ConnectionString connectionString = new ConnectionString("mongodb://" + USER + ":" + PASS + "@" + HOST + ":" + PORT + "/" + DB + "?authSource=admin");
-        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .build();
+        MongoClient mongoClient = null;
+        try {
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(new ConnectionString("mongodb://" + HOST + ":" + PORT))
+                    .build();
+            mongoClient = MongoClients.create(settings);
+            // Attempt to connect to the database
+            MongoDatabase db = mongoClient.getDatabase(DB);
+            db.listCollectionNames().first(); // Try executing a simple operation
+            isMongoDBAvailable = true;
+        } catch (Exception e) {
+            // Handle MongoDB not being available
+            System.out.println("MongoDB is not available.");
+            isMongoDBAvailable = false;
+        } finally {
+            if (mongoClient != null) {
+                mongoClient.close();
+            }
+        }
+        return mongoClient;
+    }
 
-        return MongoClients.create(mongoClientSettings);
+    public boolean isMongoDBAvailable(){
+        return  isMongoDBAvailable;
     }
 
     @Override
--- src/main/java/gng4120/group3/project/models/initializers/RoleInitializer.java
+++ src/main/java/gng4120/group3/project/models/initializers/RoleInitializer.java
@@ -1,12 +1,15 @@
 package gng4120.group3.project.models.initializers;
 
+import gng4120.group3.project.database.MongoConfig;
 import gng4120.group3.project.database.repository.RoleRepository;
 import gng4120.group3.project.models.ERole;
 import gng4120.group3.project.models.Role;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.util.Optional;
 
 @Component
@@ -15,11 +18,16 @@ public class RoleInitializer {
     @Autowired
     RoleRepository roleRepository;
 
+    @Autowired
+    private MongoConfig mongoConfig;
+
     @PostConstruct
     public void init() {
         // Iterate over each enum value and save the role if it doesn't exist
-        for (ERole roleName : ERole.values()) {
-            saveRoleIfNotExists(roleName);
+        if(mongoConfig.isMongoDBAvailable()) {
+            for (ERole roleName : ERole.values()) {
+                saveRoleIfNotExists(roleName);
+            }
         }
     }
 
